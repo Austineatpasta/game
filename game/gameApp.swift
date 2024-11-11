2fd3e831bf64225f55130c51b4c27707fa039579
Austin <austin0629@gmail.com>
2024-11-12 05:07:36 +0800

    Initial Commit

---
+++ game/gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  gameApp.swift
+//  game
+//
+//  Created by Mac on 2024/11/12.
+//
+
+import SwiftUI
+
+@main
+struct gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
